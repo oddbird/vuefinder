fe347fa7ace962ed0ef6f893b34b61fa4ba6b7a6
Miriam Suzanne <miriam@oddbird.net>
2018-06-24 01:54:11 -0600

    Allow shuffling

--- pages/talks/_talk.vue
+++ pages/talks/_talk.vue
@@ -2,28 +2,33 @@
   <main>
     <h1 v-if="title" v-html="title"></h1>
     <h2 v-if="subtitle" v-html="subtitle"></h2>
+    <button v-if="shuffleOpt" @click="shuffle">Shuffle</button>
 
     <div class="main-body" v-if="content" v-html="content"></div>
 
-    <section v-for="slide in slides"
-      :key="slide.id"
-      class="slide"
-      :style="{ backgroundImage: slide.background }">
-      <p v-if="slide.alt" hidden>{{ slide.alt }}</p>
-      <div v-if="slide.content" class="slide-body" v-html="slide.content"></div>
-    </section>
+    <transition-group name="slides" class="slides">
+      <section v-for="slide in slides"
+        :key="slide.id"
+        class="slide"
+        :style="{ backgroundImage: slide.background }">
+        <p v-if="slide.alt" v-html="slide.alt" hidden></p>
+        <div v-if="slide.content" class="slide-body" v-html="slide.content"></div>
+      </section>
+    </transition-group>
   </main>
 </template>
 
 <script>
+  import _ from 'lodash';
+
   export default {
     asyncData ({ app, params }) {
       const talk = app.context.env.talks[params.talk];
       const md = app.$md;
       const slides = talk.slides.map(slide => {
         slide.content = md.render(slide.content);
-        slide.background = `url(${slide.data.background})` || 'none';
-        slide.alt = slide.data.alt || null;
+        slide.background = slide.data.background ? `url(${slide.data.background})` : 'none';
+        slide.alt = slide.data.alt ? md.renderInline(slide.data.alt) : '';
         return slide;
       });
 
@@ -32,7 +37,16 @@
         subtitle: md.renderInline(talk.main.data.subtitle),
         content: md.render(talk.main.content),
         excerpt: md.render(talk.main.excerpt),
-        slides: slides,
+        slides: talk.main.data.shuffle ? _.shuffle(slides) : slides,
+        shuffleOpt: talk.main.data.shuffle,
+      }
+    },
+    methods: {
+      shuffle() {
+        this.slides = _.shuffle(this.slides);
+      },
+      order() {
+        this.slides = _.orderBy(this.slides, 'id', 'asc');
       }
     },
   }
@@ -60,11 +74,21 @@ pre {
   }
 }
 
+.slides {
+  display: grid;
+  grid-gap: 1em;
+  grid-template-columns: repeat(auto-fit, minmax(30em, 1fr));
+}
+
 .slide {
   background-size: cover;
   border: 1px solid #ccc;
   margin: 1em;
   min-height: 50vh;
+}
+
+.slides-move {
+  transition: all 1s .01ms ease;
 }
 
 .two-up {
