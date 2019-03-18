96698bdd23ce30f19e06945ec7f5d3953e6a8462
Miriam Suzanne <miriam@oddbird.net>
2019-03-18 17:13:56 -0600

    User Unfriendly

---
+++ pages/user-unfriendly/agiledenver19.vue
@@ -0,0 +1,30 @@
+<template>
+  <project-base :src="src" :demos="demos"/>
+</template>
+
+<script>
+// The primary stuff
+import ProjectBase from "~/components/project/ProjectBase";
+import fileSrc from "~/assets/talks/user-unfriendly/agiledenver19.slides";
+
+// Inline-Demo Components as needed
+// import cssAwesome from '~/components/demos/css-awesome';
+
+export default {
+  components: {
+    ProjectBase
+  },
+  data() {
+    return {
+      src: fileSrc,
+      demos: {
+        //   cssAwesome,
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+@import "~/assets/scss/_vuefinder.scss";
+</style>
