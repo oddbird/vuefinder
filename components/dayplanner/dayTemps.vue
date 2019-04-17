57a36022f097ca55e78edb067f721577a249ad24
Miriam Suzanne <miriam@oddbird.net>
2019-04-17 14:47:58 -0700

    Post-talk demos

--- components/dayplanner/dayTemps.vue
+++ components/dayplanner/dayTemps.vue
@@ -15,8 +15,8 @@
     </tr>
 
     <!-- Temps -->
-    <!-- :style="{'--ease': ease}" -->
-    <tr data-row="temps">
+    <!--  -->
+    <tr data-row="temps" :style="{'--ease': ease}">
 
       <!-- Temp -->
       <!-- (temp, time, index) -->
@@ -25,8 +25,9 @@
         data-cell="temp-value"
         :style="{
           '--temp': temp,
+          '--index': index,
         }"
-        v-for="(temp, time) in temps" :key="time" >
+        v-for="(temp, time, index) in temps" :key="time" >
         <span class="temp-value">
           {{ temp }}°
         </span>
@@ -41,124 +42,124 @@
 // Grid Alignment
 // --------------
 
-// .temps {
-//   display: grid;
-//   grid: auto 1fr / 1fr;
-// }
+.temps {
+  display: grid;
+  grid: auto 1fr / 1fr;
+}
 
-// [data-row] { // table header & data rows
-//   display: grid;
-//   grid-template-columns: var(--day-template);
-// }
+[data-row] { // table header & data rows
+  display: grid;
+  grid-template-columns: var(--day-template);
+}
 
-// [data-cell] { // header and data cells
-//   grid-column-end: span var(--columns-per-hour);
-// }
+[data-cell] { // header and data cells
+  grid-column-end: span var(--columns-per-hour);
+}
 
 
 // Title Orientation
 // -----------------
 
-// [data-cell='hour-title'] {
-//   writing-mode: vertical-lr;
-// }
+[data-cell='hour-title'] {
+  writing-mode: vertical-lr;
+}
 
 
 // Value Visualization
 // -------------------
 
-// [data-cell='temp-value'] {
-//   --range: calc(var(--high-temp) - var(--low-temp));
-//   display: grid;
-//   grid-template-rows: repeat(var(--range), minmax(0, 1fr));
+[data-cell='temp-value'] {
+  --range: calc(var(--high-temp) - var(--low-temp));
+  display: grid;
+  grid-template-rows: repeat(var(--range), minmax(0, 1fr));
 
-//   &::before {
-//     content: '';
-//     background: hsl(0, 75%, 50%);
-//     grid-row: calc(var(--high-temp) + 1 - var(--temp)) / -1;
-//     margin: 0 1px;
-//   }
-// }
+  &::before {
+    content: '';
+    background: hsl(0, 75%, 50%);
+    grid-row: calc(var(--high-temp) + 1 - var(--temp)) / -1;
+    margin: 0 1px;
+  }
+}
 
-// .temp-value {
-//   grid-row: -1;
-// }
+.temp-value {
+  grid-row: -1;
+}
 
 
 // Change the Scale
 // ----------------
 
-// [data-cell='temp-value'] {
-//   --range: calc(var(--high-temp) - var(--min-temp, var(--low-temp, 32)));
-//   --min-temp: calc(var(--low-temp) - var(--pad, 2));
-//   --pad: 2;
-// }
+[data-cell='temp-value'] {
+  --range: calc(var(--high-temp) - var(--min-temp, var(--low-temp, 32)));
+  --min-temp: calc(var(--low-temp) - var(--pad, 2));
+  --pad: 2;
+}
 
 
 // Add Colors
 // ----------
 
-// [data-cell='temp-value'] {
-//   &::before {
-//     --temp-fraction: calc(var(--temp) / var(--range));
-//     --hue-range: 180;
-//     --temp-hue: calc(
-//       var(--temp-fraction) * var(--hue-range)
-//     );
-//     background: hsl(var(--temp-hue, 0), 75%, 50%);
-//   }
-// }
+[data-cell='temp-value'] {
+  &::before {
+    --temp-fraction: calc(var(--temp) / var(--range));
+    --hue-range: 180;
+    --temp-hue: calc(
+      var(--temp-fraction) * var(--hue-range)
+    );
+    background: hsl(var(--temp-hue, 0), 75%, 50%);
+  }
+}
 
 
 // Improved Colors
 // ---------------
 
-// [data-cell='temp-value'] {
-//   &::before {
-//     --hue-direction: -1;
-//     --hue-offset: 90;
-//     --temp-hue: calc(
-//       var(--temp-fraction)
-//       * var(--hue-range)
-//       * var(--hue-direction)
-//       + var(--hue-offset)
-//     );
-//   }
-// }
+[data-cell='temp-value'] {
+  &::before {
+    --hue-direction: -1;
+    --hue-offset: 90;
+    --temp-hue: calc(
+      var(--temp-fraction)
+      * var(--hue-range)
+      * var(--hue-direction)
+      + var(--hue-offset)
+    );
+  }
+}
 
 
 // Graph Animation
 // ---------------
 
-// @keyframes grow-y {
-//   from {
-//     transform: scaleY(0);
-//   }
+@keyframes grow-y {
+  from {
+    transform: scaleY(0);
+  }
 
-//   to {
-//     transform: scaleY(1);
-//   }
-// }
+  to {
+    transform: scaleY(1);
+  }
+}
 
-// [data-cell='temp-value'] {
-//   &::before {
-//     animation: grow-y
-//       var(--speed, 2s)
-//       var(--delay, 0s)
-//       var(--ease, ease-in-out)
-//       both;
-//   }
-// }
+[data-cell='temp-value'] {
+  &::before {
+    animation: grow-y
+      var(--speed, 2s)
+      var(--delay, 0s)
+      var(--ease, ease-in-out)
+      both;
+  }
+}
 
 
 // Animation Origin
 // ================
 
-// [data-cell='temp-value'] {
-//   &::before {
-//     transform-origin: 50% 100%;
-//   }
-// }
+[data-cell='temp-value'] {
+  &::before {
+    transform-origin: 50% 100%;
+  }
+}
 
 
 // Animation Index Delay
@@ -166,9 +167,9 @@
 // - counter() is only valid in content property
 // - :nth-child() selectors would have to be explicit
 
-// [data-cell='temp-value'] {
-//   --delay: calc(var(--index) * 50ms);
-// }
+[data-cell='temp-value'] {
+  --delay: calc(var(--index) * 50ms);
+}
 
 
 // Animation Easing Presets
@@ -236,7 +237,7 @@
     },
     data() {
       return {
-        ease: 'ease-in'
+        ease: 'var(--out-back)'
       }
     },
     computed: {
