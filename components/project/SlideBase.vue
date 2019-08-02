c9c509aa7213bf53839c23ac3ccfb10c579e721d
Miriam Suzanne <miriam@oddbird.net>
2019-08-02 09:44:51 -0500

    Initial draft

--- components/project/SlideBase.vue
+++ components/project/SlideBase.vue
@@ -21,6 +21,7 @@
   import SlideCaption from '~/components/utility/SlideCaption.vue';
 
   import DefaultSlide from '~/components/slide/DefaultSlide.vue';
+  import LogoSlide from '~/components/slide/LogoSlide.vue';
   import TodoSlide from '~/components/slide/TodoSlide.vue';
   import TitleSlide from '~/components/slide/TitleSlide.vue';
   import ImageSlide from '~/components/slide/ImageSlide.vue';
@@ -34,6 +35,7 @@
   export default {
     components: {
       SlideCaption,
+      LogoSlide,
       TitleSlide,
       ImageSlide,
       VideoSlide,
@@ -96,6 +98,7 @@
       },
       getLayout() {
         let defaultLayout = this.slide.data.image ? 'image' : 'default';
+        defaultLayout = this.slide.data.logos ? 'logo' : defaultLayout;
         defaultLayout = this.slide.data.video ? 'video' : defaultLayout;
         defaultLayout = this.slide.data.split ? 'split' : defaultLayout;
         defaultLayout = this.slide.data.demo ? 'demo' : defaultLayout;
